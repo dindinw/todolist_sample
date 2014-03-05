954ecd1dca35e09357f4c05eaea19771d74e6fae
Alex Wu <wuyiding@gmail.com>
2014-03-05 13:55:18 +0800

    maven layout empty folders

--- classic/todolist/src/main/java/io/dindinw/todolist/domain/ToDo.java
+++ classic/todolist/src/main/java/io/dindinw/todolist/domain/ToDo.java
@@ -2,10 +2,12 @@ package io.dindinw.todolist.domain;
 
 /**
  * The ToDo Domain Object.
+ * 
  * @author alexwu
- *
+ * 
  */
 public class ToDo {
     private Long id;
-
+    private String name;
+    private String description;
 }
