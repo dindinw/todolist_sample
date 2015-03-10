164bd97f5d44240543cc0e7314ac6e26f802e1c7
Alex Wu <wuyiding@gmail.com>
2015-03-10 23:33:14 +0800

    Parser & CmdLine class, only skeleton methods

---
+++ classic/todolist/src/main/java/io/dindinw/cmdline/CmdLine.java
@@ -0,0 +1,14 @@
+package io.dindinw.cmdline;
+
+public class CmdLine {
+
+    public boolean hasOption(String optName) {
+
+        return true;
+    }
+
+    public String getOptionValue(String optName) {
+
+        return "";
+    }
+}
---
+++ classic/todolist/src/main/java/io/dindinw/cmdline/Parser.java
@@ -0,0 +1,12 @@
+package io.dindinw.cmdline;
+
+/**
+ * Created by alex on 3/10/15.
+ */
+public class Parser {
+    public static CmdLine parse(String[] args){
+        return new CmdLine();
+    }
+}
+
+
