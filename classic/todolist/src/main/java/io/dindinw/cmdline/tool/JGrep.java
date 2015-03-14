f4effebfa15a32b2681ae847c1958ed20dfe2584
Alex Wu <wuyiding@gmail.com>
2015-03-14 18:39:12 +0800

    clean up

--- classic/todolist/src/main/java/io/dindinw/cmdline/tool/JGrep.java
+++ classic/todolist/src/main/java/io/dindinw/cmdline/tool/JGrep.java
@@ -3,15 +3,7 @@ package io.dindinw.cmdline.tool;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
